201af739366a244fda09566bb52090c446af907f
Chetan kumar <chetankumar71297@gmail.com>
2023-07-16 20:21:52 +0530

    Add delete and edit functionality to the my listing section

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import Moment from "react-moment";
 import { Link } from "react-router-dom";
 import { MdLocationOn } from "react-icons/md";
-export default function ListingItem({ listing, id }) {
+import { FaTrash } from "react-icons/fa";
+import { MdEdit } from "react-icons/md";
+export default function ListingItem({ listing, id, onDelete, onEdit }) {
   return (
     <li className="relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]">
       <Link className="contents" to={`/category/${listing.type}/${id}`}>
@@ -39,7 +41,7 @@ export default function ListingItem({ listing, id }) {
           </p>
           <div className="flex items-center mt-[10px] space-x-3">
             <div className="flex items-center space-x-1">
-              <p>
+              <p className="font-bold text-xs">
                 {listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
               </p>
             </div>
@@ -53,6 +55,18 @@ export default function ListingItem({ listing, id }) {
           </div>
         </div>
       </Link>
+      {onDelete && (
+        <FaTrash
+          className="absolute bottom-2 right-2 h-[14px] cursor-pointer text-red-500"
+          onClick={() => onDelete(id)}
+        />
+      )}
+      {onEdit && (
+        <MdEdit
+          className="absolute bottom-2 right-7 h-4 cursor-pointer "
+          onClick={() => onEdit(id)}
+        />
+      )}
     </li>
   );
 }
